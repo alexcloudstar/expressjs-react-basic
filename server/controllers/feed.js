aa2d631b04e7afa9a9f4b8ed4b384d888bc2a5a4
Alex Cloudstar <58813782+alexcloudstar@users.noreply.github.com>
2021-01-13 21:21:32 +0200

    Added PATCH/DELETE posts in TODO

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -73,3 +73,34 @@ exports.createPost = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        console.log('Error:!!', error);
+        error.statusCode = 404;
+        throw error;
+      }
+
+      res.status(200).json({ message: 'Post Fetched.', post: post });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+
+      next(err);
+    });
+};
+
+exports.updatePost = (req, res, next) => {
+  // TODO: Develope feature
+};
+
+exports.deletePost = (req, res, next) => {
+  // TODO: Develope feature
+};
--- server/routes/feed.js
+++ server/routes/feed.js
@@ -8,7 +8,16 @@ const router = express.Router();
 // @ GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
+// @ GET /feed/post/:postId
+router.get('/post/:postId', feedController.getPost);
+
 // @ POST /feed/post
 router.post('/post', feedController.createPost);
+
+// @ PATCH /feed/post
+router.patch('/post/:postId', feedController.updatePost);
+
+// @ PATCH /feed/post
+router.delete('/post/:postId', feedController.deletePost);
 
 module.exports = router;
