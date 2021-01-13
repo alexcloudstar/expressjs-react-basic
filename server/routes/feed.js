4ee68c98c747cfe88891c2f1063033ca6d3ade80
Alex Cloudstar <58813782+alexcloudstar@users.noreply.github.com>
2021-01-13 20:26:33 +0200

    added controllers & models for posts

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { body } = require('express-validator');
 
+const feedController = require('../controllers/feed');
+
 const router = express.Router();
 
 // @ GET /feed/posts
-router.get('/posts', (req, res, next) => {
-  console.log('Im on /');
-});
+router.get('/posts', feedController.getFeed);
 
 module.exports = router;
