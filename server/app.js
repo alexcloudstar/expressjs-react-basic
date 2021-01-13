5a236d3be89cbfb4edd53e96cdf5af587aa0fc63
Alex Cloudstar <58813782+alexcloudstar@users.noreply.github.com>
2021-01-13 20:16:32 +0200

    Added dotenv package, .env.template && set up the server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,51 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config();
+
+const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-app.listen(8080);
+app.use(bodyParser.json());
 
-console.log('App listening 8080');
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Authorization'
+  );
+
+  next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(result => {
+    app.listen(8080);
+    console.log('Connected to DB');
+  })
+  .catch(err => console.log(err));
