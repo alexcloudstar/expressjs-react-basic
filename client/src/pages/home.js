c8d57a3169b8302b85473bd1fb9d6198b6c6576d
Alex Cloudstar <58813782+alexcloudstar@users.noreply.github.com>
2021-01-13 09:33:15 +0200

    Added header/navbar

---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../components/header/index';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div className='home'>Home</div>
+    </>
+  );
+};
+
+export default Home;
