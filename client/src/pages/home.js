73383eed23e548a757ff8e5924692406436b9a24
Alex Cloudstar <58813782+alexcloudstar@users.noreply.github.com>
2021-01-13 09:41:50 +0200

    Created homepage

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,61 @@
 import React from 'react';
 import { Header } from '../components/header/index';
+import styled from 'styled-components';
+
+const Feed = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+
+  h3 {
+    color: #229ae6;
+    font-size: 25px;
+    font-weight: 600;
+  }
+`;
+
+const FeedPosts = styled.div`
+  h4,
+  h5,
+  h6 {
+    margin: 4px 0;
+    text-align: left;
+  }
+
+  h4 {
+    font-size: 20px;
+    font-weight: 600;
+    color: yellow;
+  }
+
+  h5,
+  h6 {
+    font-size: 18px;
+  }
+
+  img {
+    margin: 5px 0;
+  }
+`;
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div className='home'>Home</div>
+      <div className='home'>
+        <Feed>
+          <h3>Feed</h3>
+          <FeedPosts>
+            <h4>This is an awesome post title!</h4>
+            <h5>Created by: Alex Cloudstar</h5>
+            <h6>Date: 01-01-2020</h6>
+            <img alt='post-title' src='https://via.placeholder.com/150' />
+            <p>Here's an awesome description</p>
+          </FeedPosts>
+        </Feed>
+      </div>
     </>
   );
 };
