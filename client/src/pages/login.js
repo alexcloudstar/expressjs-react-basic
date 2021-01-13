1528e2dedae5017b4f318e595da6bcee82b79a3a
Alex Cloudstar <58813782+alexcloudstar@users.noreply.github.com>
2021-01-13 19:45:39 +0200

    Added buttons on register/login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -8,12 +8,11 @@ export const Login = () => {
     <>
       <LoginDiv>
         <h1>Login</h1>
-        <form action=''>
-          <label htmlFor='email'>Email</label>
-          <input type='email' name='email' />
-          <label htmlFor='password'>Password</label>
-          <input type='password' name='password' />
-        </form>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' />
+        <button type='submit'>Login</button>
       </LoginDiv>
     </>
   );
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -8,14 +8,13 @@ export const Register = () => {
     <>
       <RegisterDiv>
         <h1>Register</h1>
-        <form action=''>
-          <label htmlFor='email'>Email</label>
-          <input type='email' name='email' id='email' />
-          <label htmlFor='name'>Name</label>
-          <input type='text' name='name' id='name' />
-          <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' />
-        </form>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' />
+        <label htmlFor='name'>Name</label>
+        <input type='text' name='name' id='name' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' id='password' />
+        <button type='submit'>Login</button>
       </RegisterDiv>
     </>
   );
