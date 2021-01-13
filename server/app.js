358d836837b72a6719c3035b5fbe34648859af83
Alex Cloudstar <58813782+alexcloudstar@users.noreply.github.com>
2021-01-13 21:27:44 +0200

    Added one image && setup multer

--- server/app.js
+++ server/app.js
@@ -11,7 +11,35 @@ const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json());
+
+app.use(
+  multer({
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single('image')
+);
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
